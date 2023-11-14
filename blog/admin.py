8c04645c82430a0e55106b49268c2b11dca2f74e
johnrearden <isnocturnal@gmail.com>
2023-11-14 10:12:59 +0000

    Add codestart walkthrough templates and css

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Post, Comment
-# Register your models here.
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content',]
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',) }
+    summernote_fields = ('content', )
+
 admin.site.register(Comment)   
